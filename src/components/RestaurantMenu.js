81eedcca2c98eab449131451521d908cc8dc230c
sudip paladhi <sudippaladhi@gmail.com>
2023-09-25 22:59:25 +0530

    API Updated

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -18,11 +18,11 @@ const RestaurantMenu = () => {
     avgRatingString,
   } = restaurantInfo?.cards[0]?.card?.card?.info;
   const categories =
-    restaurantInfo?.cards?.map((item)=>{item?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    restaurantInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (x) =>
         x.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-    )})
+    );
 
   return (
     <>
